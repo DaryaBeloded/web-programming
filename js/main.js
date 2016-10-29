36fbef03304316afec9328ff60f877e9f4c0e23c
Beloded Dasha <darya_np@mail.ru>
2016-10-29 14:46:01 +0300

    verion 3

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
-"use strict";
+'use strict';
+
+
 
 $(document).ready(function () {
-    $.ajax({ 
+    Promise.resolve(
+    $.ajax({
         type: 'GET',
         url:'https://api.worldoftanks.ru/wot/account/info/?application_id=9984e7e11df63b8bf2f931be4078b451&account_id=291,588,782,555,999',
-        success:function(players) { 
+    })
+    )
+    .then((players)=>{
             console.log(players);
-            var template = Handlebars.compile( $('#template').html()  );
+            let template = Handlebars.compile( $('#template').html()  );
             $('.updates').append( template(players)  );
-        },
-        error:function() {
-            console.log("error"); 
-        }
-    });
-    setInterval("location.reload()", 1000000);
-})
+    })
+    .catch ((e)=>{
+        console.log("error"); 
+    })
+        setInterval("location.reload()", 100000);
+    })
+    
+
